ca889615058cf64c2b01b394b53f517883994750
khi8194 <khi8194@gmail.com>
2024-09-20 16:21:31 +0900

    버튼 클릭시 좌우 슬라이드 이동

--- js/anime.js
+++ js/anime.js
@@ -20,12 +20,12 @@ class Anime {
 	}
 	getValue(key, value, type) {
 		let currentValue = null;
-		// currentValue = parseFloat(getComputedStyle(this.selector)[key]);
-		currentValue = this.selector.scrollY;
+		currentValue = parseFloat(getComputedStyle(this.selector)[key]);
+		// currentValue = this.selector.scrollY;
 		key === "scroll"
 			? (currentValue = this.selector.scrollY)
-			: //(currentValue = parseFloat(getComputedStyle(this.selector)[key]));
-			  (currentValue = this.selector.scrollY);
+			: (currentValue = parseFloat(getComputedStyle(this.selector)[key]));
+		// currentValue = this.selector.scrollY;
 		if (type === "percent") {
 			const parentW = parseInt(
 				getComputedStyle(this.selector.parentElement).width
