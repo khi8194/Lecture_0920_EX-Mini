049fb71a25de76a06a870edd53efae67643d0ca3
khi8194 <khi8194@gmail.com>
2024-09-20 16:52:13 +0900

    패널과 버튼의 갯수에 따라 너비, 높이 스타일링 js가 동적으로 처리

--- js/script.js
+++ js/script.js
@@ -8,13 +8,24 @@ const frame = document.querySelector("main");
 const panel = frame.querySelector(".panel");
 const btns = frame.querySelectorAll(".btns li");
 
+//html구조에서 패널과 버튼의 개수가 바뀔때마다 일일이 css파일에서
+//넓이, 높이값을 재설정하는 번거로움 피하기 위함
+
+//스크립트가 실행되자마자 panel의 넓이값과 높이값을
+//btns의 개수에 따라 동적 스타일링
+panel.style.width = 100 * btns.length + "%";
+panel.style.height = "100%";
+
+//스크립트가 실행되자마자 panel의 자식 li를 모두 찾은 다음
+//반복을 돌며 btns의 개수에 따라 넓이, 높이 동적 스타일링
+panel.querySelectorAll("li").forEach((li) => {
+	li.style.width = 100 / btns.length + "%";
+	li.style.height = "100%";
+});
+
 btns.forEach((btn, idx) => {
 	btn.addEventListener("click", () => {
 		console.log(idx);
-		// new Anime(panel, { marginLeft: -400 * idx }, { duration: 500 });
-		// new Anime(panel, { marginLeft: -100 * idx + "%" }, { duration: 500 });  //margin, padding 퍼센트 모션 불가 오류 발생
 		new Anime(panel, { left: -100 * idx + "%" }, { duration: 500 });
 	});
 });
-
-//미션 : HTML에서 li의 개수만 수정하면 scss에서 일일이 panel, li의 넓이를 수정할 필요가 없도록 스크립트에서 동적 처리
